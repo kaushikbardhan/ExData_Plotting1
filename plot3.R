50e13138e180e4e7591b15bb5dbb646c6cc7ae9c
Kaushik Bardhan <kaushik.bardhan@gmail.com>
2016-03-14 01:03:54 -0500

    Added files via upload

---
+++ plot3.R
@@ -0,0 +1,32 @@
+data <- read.table("household_power_consumption.txt",header=TRUE, sep=";",
+                   stringsAsFactors=FALSE)
+
+data$Date <- as.Date(as.character(data$Date),format="%d/%m/%Y")
+
+DATE1 <- as.Date("2007-02-01")
+DATE2 <- as.Date("2007-02-02")
+
+pd <- subset(data, Date >= DATE1 & Date <= DATE2)
+
+pd$Global_active_power <- as.numeric(pd$Global_active_power)
+pd$Voltage <- as.numeric(pd$Voltage)
+pd$Global_reactive_power <- as.numeric(pd$Global_reactive_power)
+pd$Sub_metering_1 <- as.numeric(pd$Sub_metering_1)
+pd$Sub_metering_2 <-as.numeric(pd$Sub_metering_2)
+pd$Global_intensity <- as.numeric(pd$Global_intensity)
+
+pd$dtm <- as.POSIXct(paste(pd$Date, pd$Time), format="%Y-%m-%d %H:%M:%S")
+
+with(pd, plot(dtm,Sub_metering_1,ylab = "Energy sub metering",type="n"))
+with(pd, lines(dtm,Sub_metering_1, col="black")) 
+with(pd, lines(dtm,Sub_metering_2, col="red")) 
+with(pd, lines(dtm,Sub_metering_3, col="blue"))
+legend("topright",lty=1 , lwd=1, col = c("black","red", "blue"), 
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+
+dev.copy(png,file="plot3.png", width=480, height=480, units = "px")
+
+dev.off()
+
+
