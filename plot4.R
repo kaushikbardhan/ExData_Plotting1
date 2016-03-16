a0aedd1f163718fb95d3907e54d7b981629c1558
Kaushik Bardhan <kaushik.bardhan@gmail.com>
2016-03-15 22:43:45 -0500

    Added files via upload

--- plot4.R
+++ plot4.R
@@ -23,7 +23,7 @@ par(mfrow = c(2, 2))
 with(pd, {
  plot(datetime,Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
  plot(datetime,Voltage, type="l")  
-  plot(datetime,Sub_metering_1, xlab="", ylab = "Energy sub metering",col="black")
+  plot(datetime,Sub_metering_1, type="l", xlab="", ylab = "Energy sub metering",col="black")
         lines(datetime,Sub_metering_2, col="red")
         lines(datetime,Sub_metering_3, col="blue")
         legend("topright",bty="n" , lwd=1, col = c("black","red", "blue"), 
